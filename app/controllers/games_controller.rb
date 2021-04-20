a3b0b4d1900f18e8df051be051719e3f8983e45c
Doug <doug.kerzner@gmail.com>
2021-04-19 22:51:26 -0400

    Added Routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+
+    def index
+        @games = Game.all
+    end
+
+    def gameover
+        @game = params.permit!()
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # resources :game_questions
-  # resources :games
+  resources :games
   # resources :users
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post '/questions/choose_game', to: 'questions#choosegame'
+  post '/games/game_over', to: 'games#gameover'
 end
