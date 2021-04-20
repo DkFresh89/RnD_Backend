f59e12eb959b1de8722f3d574cc28c9068551d35
Doug <doug.kerzner@gmail.com>
2021-04-19 23:17:35 -0400

    Custom Routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,21 @@
 class GamesController < ApplicationController
 
-    def index
-        @games = Game.all
-    end
+    # def index
+    #     @games = Game.all
+
+    #     render json: @games
+    # end
 
     def gameover
         @game = params.permit!()
+        # byebug
+        
+
+        
+
+        # Game.create(@game)
+
+        render json: @game
     end
 
 end
