9b5ef53e74fb02d733444d6a3b64ec629671c7cc
Doug <doug.kerzner@gmail.com>
2021-06-03 19:27:32 -0400

    Fixing serializers

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,5 +2,5 @@ class QuestionSerializer
   include JSONAPI::Serializer
   attributes :category, :type, :difficulty, :question, :correct_answer, :incorrect_answers, :points
 
-  serialize :incorrect_answers, Array
+  # serialize :incorrect_answers, Array
 end
